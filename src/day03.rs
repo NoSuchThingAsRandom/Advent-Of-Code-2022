a7d938026945ceaa1497be6238c415b5b8a29366
Sam <s@a.com>
2022-12-04 13:43:14 +0000

    Completed day03

--- src/day03.rs
+++ src/day03.rs
@@ -1,61 +1,74 @@
+use std::collections::HashSet;
 use std::fs::File;
 use std::io::{BufRead, BufReader};
 use anyhow::Context;
 use crate::{EXAMPLE_INPUT_PATH, INPUT_PATH};
+use crate::misc::read_vec_string;
 
 
 pub fn part_1(use_example_input: bool) -> anyhow::Result<usize> {
     let filename = if use_example_input {
-        EXAMPLE_INPUT_PATH.to_owned() + "01/A.txt"
+        EXAMPLE_INPUT_PATH.to_owned() + "03/A.txt"
     } else {
-        INPUT_PATH.to_owned() + "01.txt"
+        INPUT_PATH.to_owned() + "03.txt"
     };
-    let file = File::open(filename.to_string()).context(format!("Failed to file {}", filename))?;
-    let reader = BufReader::new(file);
-    let mut current_elf_index = 1;
-    let mut current_calorie_count = 0;
-    let mut most_calories = 0;
-    let mut most_calories_index = 0;
-    for line in reader.lines() {
-        let line = line?;
-        if line.is_empty() {
-            if current_calorie_count > most_calories {
-                most_calories = current_calorie_count;
-                most_calories_index = current_elf_index;
+    let data = read_vec_string(filename)?;
+    let mut score = 0;
+    for line in data {
+        let (one, two) = line.split_at(line.len() / 2);
+        let one: HashSet<char> = HashSet::from_iter(one.chars());
+        let two: HashSet<char> = HashSet::from_iter(two.chars());
+        let duplicates = one.intersection(&two);
+        for char in duplicates {
+            let mut char_int = *char as u32;
+            if char_int < 91 {
+                char_int -= 65;
+                char_int += 27;
+            } else {
+                char_int -= 97;
+                char_int += 1;
             }
-            current_elf_index += 1;
-            current_calorie_count = 0;
-        } else {
-            let calories: usize = line.parse()?;
-            current_calorie_count += calories;
+            //print!("{} -> {},\t", char, char_int);
+            score += char_int as usize;
         }
+        /*        for char in one.chars() {
+                    if two.contains(&char){
+                        println!("'{}' -> '{}'",char,char as u32);
+                    }
+                }*/
     }
-    Ok(most_calories)
+    Ok(score)
 }
 
 pub fn part_2(use_example_input: bool) -> anyhow::Result<usize> {
     let filename = if use_example_input {
-        EXAMPLE_INPUT_PATH.to_owned() + "01/A.txt"
+        EXAMPLE_INPUT_PATH.to_owned() + "03/A.txt"
     } else {
-        INPUT_PATH.to_owned() + "01.txt"
+        INPUT_PATH.to_owned() + "03.txt"
     };
-    let file = File::open(filename.to_string()).context(format!("Failed to file {}", filename))?;
-    let reader = BufReader::new(file);
-    let mut current_calorie_count = 0;
-    let mut calories_per_elf = Vec::new();
-    for line in reader.lines() {
-        let line = line?;
-        if line.is_empty() {
-            calories_per_elf.push(current_calorie_count);
-            current_calorie_count = 0;
-        } else {
-            let calories: usize = line.parse()?;
-            current_calorie_count += calories;
+    let data = read_vec_string(filename)?;
+    let mut score = 0;
+    let mut elf_group: Vec<HashSet<char>> = Vec::with_capacity(3);
+    for line in data {
+        elf_group.push(HashSet::from_iter(line.chars()));
+        if elf_group.len() == 3 {
+            let a = elf_group[1].intersection(&elf_group[2]);
+            let b: HashSet<char> = a.map(|x| *x).collect();
+            let unique = elf_group[0].intersection(&b);
+            for char in unique {
+                let mut char_int = *char as u32;
+                if char_int < 91 {
+                    char_int -= 65;
+                    char_int += 27;
+                } else {
+                    char_int -= 97;
+                    char_int += 1;
+                }
+                //println!("{} -> {},\t", char, char_int);
+                score += char_int as usize;
+            }
+            elf_group = Vec::with_capacity(3);
         }
     }
-    calories_per_elf.push(current_calorie_count);
-    calories_per_elf.sort_unstable();
-    calories_per_elf.reverse();
-    let total = calories_per_elf[0] + calories_per_elf[1] + calories_per_elf[2];
-    Ok(total)
+    Ok(score)
 }
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 mod day01;
 mod day02;
 mod misc;
+mod day03;
 
 pub const EXAMPLE_INPUT_PATH: &str = "example_inputs/day";
 pub const INPUT_PATH: &str = "inputs/day";
@@ -9,6 +10,7 @@ fn main() {
     /*println!("Day 1 Part 1: {}", day01::part_1(false).unwrap());
     println!("Day 1 Part 1: {}", day01::part_2(false).unwrap());*/
     //println!("Day 2 Part 1: {}", day02::part_1(false).unwrap());
-    println!("Day 2 Part 2: {}", day02::part_2(false).unwrap());
     //println!("Day 2 Part 2: {}", day02::part_2(false).unwrap());
+    //println!("Day 3 Part 1: {}", day03::part_1(false).unwrap());
+    println!("Day 3 Part 2: {}", day03::part_2(false).unwrap());
 }
